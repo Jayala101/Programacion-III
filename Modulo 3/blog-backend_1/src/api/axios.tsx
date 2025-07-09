2f1f7385733db9355287a27e4a725544902dea4a
Jayala101 <jimmysanti1@hotmail.com>
2025-07-08 21:43:52 -0500

    remove backend

--- Modulo 3/blog-backend_1/src/api/axios.tsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  //baseURL: import.meta.env.VITE_API_URL,
-//  baseURL: process.env.REACT_APP_API_URL,
- baseURL: process.env.VITE_API_URL,
-});
-
-export default axiosInstance;
